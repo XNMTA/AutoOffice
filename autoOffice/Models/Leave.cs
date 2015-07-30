a95f1de7c2a727a7fe13e9d3824df643bcae1bdf
zbo2@qq.com <zbo2@qq.com>
2015-07-30 14:05:39 +0800

    add models

---
+++ autoOffice/Models/Leave.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace autoOffice.Models
+{
+    public enum LeaveApproveStatus{
+        Approved,
+        Reject,
+        Pending
+    }
+    public class Leave
+    {
+        public Guid ID { get; set; }
+        public float Hours { get; set; }
+        public DateTime FromDate { get; set; }
+        public Guid EmployId { get; set; }
+        public String Comments { get; set; }
+        public LeaveApproveStatus status { get; set; }
+        public string Approver { get; set; }
+    }
+
+    public class LeaveDBContext : DbContext
+    {
+        public DbSet<Leave> Leaves { get; set; }
+
+    }
+}
