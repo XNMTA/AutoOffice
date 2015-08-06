d16f8681fc0722c3e7a6f54d0968cc1b8a1caeff
zbo2@qq.com <zbo2@qq.com>
2015-08-06 11:32:18 +0800

    add nav

---
+++ autoOffice/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using autoOffice.Common;
+using autoOffice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace autoOffice.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
