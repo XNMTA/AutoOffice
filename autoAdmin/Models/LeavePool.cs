e435e6b1ebf0cca1e6383e6ad58aa6723c60eefb
zbo2@qq.com <zbo2@qq.com>
2015-08-12 14:13:26 +0800

    add leave pool

---
+++ autoAdmin/Models/LeavePool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace autoOffice.Models
+{
+    [Table("rc_LeavePools", Schema = "dbo")]
+    public class LeavePool
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public double Hours { get; set; }
+        [MaxLength(50)]
+        public String EmployName { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
